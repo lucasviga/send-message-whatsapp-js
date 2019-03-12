b17b9d11ac1432fe23d575b3953f5a13f751e00d
Lucas Viga <lucasviga12@gmail.com>
2019-03-12 17:04:55 -0400

    new transitions effects

--- msg-wpp/js/sendMsg.js
+++ msg-wpp/js/sendMsg.js
@@ -4,6 +4,9 @@ function show(){
   var div = document.getElementById('box-wpp');
   div.style.display = "block";
 
+  document.getElementsByClassName('pulse')[0].style.width="20px";
+  document.getElementsByClassName('pulse')[0].style.height="20px";
+  
   var mobileDetect = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 
   if(mobileDetect){
@@ -17,6 +20,9 @@ function show(){
 function fechar(){
   var div = document.getElementById('box-wpp');
   div.style.display = "none";
+
+  document.getElementsByClassName('pulse')[0].style.width="42px";
+  document.getElementsByClassName('pulse')[0].style.height="42px"
 }
 
 function send(){
